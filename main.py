aa8d93c54fd097e5f5275922617a05f98a7b5ac4
andresq99 <andres.quelal@yachaytech.edu.ec>
2023-02-07 21:35:05 -0500

    Request and Responde body

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
 #Se importa el modulo FastAPi de la libreria fastapi
 from fastapi import FastAPI
+from fastapi import Body
 
 #Se crea una instancia de la clase FastAPI
 app = FastAPI()
+
+# Models
+
+class Person(BaseModel):
+    first_name : str
+    last_name : str
+    age: int
+    hair_color: Optional[str] = None # Valores opcionales
+    is_married: Optional[bool] = None # Valores opcionales
+
+
 
 #Se crea un path operation decorator usando la funcion get
 #En el home de la aplicacion se ejecutara nuestra funcion
@@ -10,3 +28,9 @@ app = FastAPI()
 @app.get("/") #Path operation decorator: Permite que la operation function acceda a una path ejecutando la definicion de la funcion
 def home(): # Path operation function
     return {"message": "Hello World"}
+
+# Request and Responde Body
+
+@app.post("/person/new") # Enviar datos desde el cliente al servidor
+def create_person(person: Person = Body(...)):
+    return person

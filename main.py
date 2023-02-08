8c5da20dffcc01946940a72bf60185c64eac4bb3
andresq99 <andres.quelal@yachaytech.edu.ec>
2023-02-07 21:11:10 -0500

    Second Commit Comments

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app = FastAPI()
 #Se crea un path operation decorator usando la funcion get
 #En el home de la aplicacion se ejecutara nuestra funcion
 
-@app.get("/")
-def home():
+@app.get("/") #Path operation decorator: Permite que la operation function acceda a una path ejecutando la definicion de la funcion
+def home(): # Path operation function
     return {"message": "Hello World"}

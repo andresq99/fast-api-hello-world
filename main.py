ebe3d7258fa7cc5fcf9e55acff32ea359d11ee17
andresq99 <andres.quelal@yachaytech.edu.ec>
2023-02-08 00:17:52 -0500

    automatic_examples

--- main.py
+++ main.py
@@ -85,12 +85,14 @@ def show_person(
         min_length=1, 
         max_length=50,
         title = "Perso Name",
-        description= "This is the person name. It´s between 1 and 50 characters"
+        description= "This is the person name. It´s between 1 and 50 characters",
+        example="Rocio"
     ),
     age: str = Query(
         ..., 
         title="Person Age",
-        description="This is the person age. Its required"
+        description="This is the person age. Its required",
+        example=25
     )
 ):
     return {name : age}
@@ -102,6 +104,7 @@ def show_person(
 def show_person(
     person_id: int = Path(..., 
                           gt=0,
+                          example=123,
                           title="Person Id",
                           description="Showing person id. Its required")
 ):
@@ -115,7 +118,8 @@ def update_person(
         ...,
         title ="Person ID",
         description = "This is the person ID",
-        gt=0
+        gt=0,
+        example=123
     ),
     # Path operation (Que nos envio 2 request body)
     person: Person = Body(...),
